3009675695e24bd0f1e77283a412fd64b721c09e
wildp <git@wildp.net>
2026-03-03 23:14:53 +0000

    Improve benchmarks

--- bench/twain.cpp
+++ bench/twain.cpp
@@ -8,25 +8,51 @@ static const char8_t data[] = {
 , '\0' /* null terminator */
 };
 
+static const std::string_view input{ reinterpret_cast<const char*>(data) };
+
 template<typename Matcher>
 static void BM_ctre(benchmark::State& state, Matcher m)
 {
-    const auto input{ std::ranges::subrange(data, rx::detail::cstr_sentinel) };
+    const char* cstr{ input.data() };
     for (auto _ : state)
     {
-        auto range{ m.range(input) };
+        auto range{ m.range(cstr) };
         for (auto result : range)
             benchmark::DoNotOptimize(result);
     }
 }
 
-template<typename Matcher>
-static void BM_rx(benchmark::State& state, Matcher m)
+template<rx::string_literal Pattern>
+static void BM_rx(benchmark::State& state, rx::static_regex<Pattern> m)
 {
-    const auto input{ std::ranges::subrange(data, rx::detail::cstr_sentinel) };
+    const char* cstr{ input.data() };
     for (auto _ : state)
     {
-        auto range{ input | rx::views::regex_match(m) };
+        auto range{ m.range(cstr) };
+        for (auto result : range)
+            benchmark::DoNotOptimize(result);
+    }
+}
+
+template<rx::string_literal Pattern>
+static void BM_rxf(benchmark::State& state, rx::static_regex<Pattern, rx::mode::fast> m)
+{
+    const char* cstr{ input.data() };
+    for (auto _ : state)
+    {
+        auto range{ m.range(cstr) };
+        for (auto result : range)
+            benchmark::DoNotOptimize(result);
+    }
+}
+
+template<rx::string_literal Pattern>
+static void BM_rxn(benchmark::State& state, rx::static_regex<Pattern, rx::mode::naive> m)
+{
+    const char* cstr{ input.data() };
+    for (auto _ : state)
+    {
+        auto range{ m.range(cstr) };
         for (auto result : range)
             benchmark::DoNotOptimize(result);
     }
@@ -35,8 +61,15 @@ static void BM_rx(benchmark::State& state, Matcher m)
 using namespace ctre::literals;
 using namespace rx::literals;
 
-#define TEST(PATTERN)                                \
-BENCHMARK_CAPTURE(BM_rx, PATTERN, PATTERN ## _rx);   \
+#define TEST(PATTERN)                                  \
+BENCHMARK_CAPTURE(BM_rx, PATTERN, PATTERN ## _rx);     \
+BENCHMARK_CAPTURE(BM_rxf, PATTERN, PATTERN ## _rxf);   \
+BENCHMARK_CAPTURE(BM_rxn, PATTERN, PATTERN ## _rxn);   \
+BENCHMARK_CAPTURE(BM_ctre, PATTERN, PATTERN ## _ctre);
+
+#define TEST_NO_FAST(PATTERN)                          \
+BENCHMARK_CAPTURE(BM_rx, PATTERN, PATTERN ## _rx);     \
+BENCHMARK_CAPTURE(BM_rxn, PATTERN, PATTERN ## _rxn);   \
 BENCHMARK_CAPTURE(BM_ctre, PATTERN, PATTERN ## _ctre);
 
 TEST(R"(Twain)");
@@ -44,12 +77,12 @@ TEST(R"((?i)Twain)");
 TEST(R"([a-z]shing)");
 TEST(R"(Huck[a-zA-Z]+|Saw[a-zA-Z]+)");
 TEST(R"(\b\w+nn\b)");
-// TEST(R"([a-q][^u-z]{13}x)");
+TEST_NO_FAST(R"([a-q][^u-z]{13}x)");
 TEST(R"(Tom|Sawyer|Huckleberry|Finn)");
-TEST(R"((?i)Tom|Sawyer|Huckleberry|Finn)");
-TEST(R"(.{0,2}(Tom|Sawyer|Huckleberry|Finn))");
-TEST(R"(.{2,4}(Tom|Sawyer|Huckleberry|Finn))");
-// TEST(R"(Tom.{10,25}river|river.{10,25}Tom)");
+TEST(R"((?i)(?:Tom|Sawyer|Huckleberry|Finn))");
+TEST(R"(.{0,2}(?:Tom|Sawyer|Huckleberry|Finn))");
+TEST(R"(.{2,4}(?:Tom|Sawyer|Huckleberry|Finn))");
+TEST_NO_FAST(R"(Tom.{10,25}river|river.{10,25}Tom)");
 TEST(R"([a-zA-Z]+ing)");
 TEST(R"(\s[a-zA-Z]{0,12}ing\s)");
 TEST(R"(([A-Za-z]awyer|[A-Za-z]inn)\s)");
