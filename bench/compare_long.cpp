c6790c1df74506e075f20f30d4c065e4009d60d3
wildp <git@wildp.net>
2026-05-21 19:39:24 +0100

    Add Boost.Xpressive to comparisons

--- bench/compare_long.cpp
+++ bench/compare_long.cpp
@@ -4,6 +4,7 @@
 #include <regex>
 #include <re2/re2.h>
 #include <boost/regex.hpp>
+#include <boost/xpressive/xpressive_static.hpp>
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
@@ -18,6 +19,12 @@ static const std::string_view input{ reinterpret_cast<const char*>(data) };
 
 namespace
 {
+    template<srx::string_literal Pattern>
+    struct xpr_pattern {};
+
+    template<srx::string_literal Pattern>
+    consteval xpr_pattern<Pattern> operator ""_xpr() { return {}; }
+
     template<typename Matcher>
     std::size_t matchcount_ctre(Matcher re)
     {
@@ -57,6 +64,50 @@ namespace
             ++count;
 
         return count;
+    }
+
+    template<srx::string_literal Pattern>
+    std::size_t matchcount_xpr(const xpr_pattern<Pattern>)
+    {
+        auto re = []() -> boost::xpressive::cregex {
+            using namespace boost::xpressive;
+            if constexpr (Pattern.view() == R"(Twain)")
+                return as_xpr("Twain");
+            else if constexpr (Pattern.view() == R"((?i)Twain)")
+                return icase("Twain");
+            else if constexpr (Pattern.view() == R"([a-z]shing)")
+                return range('a', 'z') >> "shing";
+            else if constexpr (Pattern.view() == R"(Huck[a-zA-Z]+|Saw[a-zA-Z]+)")
+                return ("Huck" >> +set[range('a', 'z') | range('A', 'Z')]) | ("Saw" >> +set[range('a', 'z') | range('A', 'Z')]);
+            else if constexpr (Pattern.view() == R"(\b\w+nn\b)")
+                return _b >> +_w >> "nn" >> _b;
+            else if constexpr (Pattern.view() == R"([a-q][^u-z]{13}x)")
+                return range('a', 'q') >> repeat<13, 13>(~range('u', 'z')) >> "x";
+            else if constexpr (Pattern.view() == R"(Tom|Sawyer|Huckleberry|Finn)")
+                return as_xpr("Tom") | "Sawyer" | "Huckleberry" | "Finn";
+            else if constexpr (Pattern.view() == R"((?i)(?:Tom|Sawyer|Huckleberry|Finn))")
+                return icase(as_xpr("Tom") | "Sawyer" | "Huckleberry" | "Finn");
+            else if constexpr (Pattern.view() == R"(.{0,2}(?:Tom|Sawyer|Huckleberry|Finn))")
+                return repeat<0, 2>(~(set = '\n')) >> (as_xpr("Tom")| "Sawyer" | "Huckleberry" | "Finn");
+            else if constexpr (Pattern.view() == R"(.{2,4}(?:Tom|Sawyer|Huckleberry|Finn))")
+                return repeat<2, 4>(~(set = '\n')) >> (as_xpr("Tom") | "Sawyer" | "Huckleberry" | "Finn");
+            else if constexpr (Pattern.view() == R"(Tom.{10,25}river|river.{10,25}Tom)")
+                return ("Tom" >> repeat<10, 25>(~(set = '\n')) >> "river") | ("river" >> repeat<10, 25>(~(set = '\n')) >> "Tom");
+            else if constexpr (Pattern.view() == R"([a-zA-Z]+ing)")
+                return +set[range('a', 'z') | range('A', 'Z')] >> "ing";
+            else if constexpr (Pattern.view() == R"(\s[a-zA-Z]{0,12}ing\s)")
+                return _s >> repeat<0, 12>(set[range('a', 'z') | range('A', 'Z')]) >> "ing" >> _s;
+            else if constexpr (Pattern.view() == R"((?:[A-Za-z]awyer|[A-Za-z]inn)\s)")
+                return ((set[range('A', 'Z') | range('a', 'z')] >> "awyer") | (set[range('A', 'Z') | range('a', 'z')] >> "inn")) >> _s;
+            else if constexpr (Pattern.view() == R"(["'][^"']{0,30}[?!\.][\"'])")
+                return (set = '\"', '\'') >> repeat<0,30>(~(set = '\"', '\'')) >> (set= '?','!','.') >> (set = '\"', '\'');
+            else
+                static_assert("Unknown Pattern");
+        }();
+
+        boost::xpressive::cregex_iterator it{ input.begin(), input.end(), re };
+        boost::xpressive::cregex_iterator end{};
+        return std::ranges::distance(it, end);
     }
 
     std::size_t matchcount_pcre2(const std::string_view pattern)
@@ -190,6 +241,19 @@ static void BM_re2(benchmark::State& state, const std::string_view pattern, cons
     }
 }
 
+template<typename Matcher>
+static void BM_xpr(benchmark::State& state, Matcher re, const std::size_t count)
+{
+    if (matchcount_xpr(re) != count)
+        return;
+
+    for (auto _ : state)
+    {
+        auto tmp = matchcount_xpr(re);
+        benchmark::DoNotOptimize(tmp);
+    }
+}
+
 static void BM_pcre2(benchmark::State& state, const std::string_view pattern, const std::size_t count)
 {
     if (matchcount_pcre2(pattern) != count)
@@ -221,6 +285,7 @@ using namespace srx::literals;
 BENCHMARK_CAPTURE(BM_srx, PATTERN, PATTERN ## _srx, COUNT);    \
 BENCHMARK_CAPTURE(BM_ctre, PATTERN, PATTERN ## _ctre, COUNT);\
 BENCHMARK_CAPTURE(BM_re2, PATTERN, PATTERN, COUNT);          \
+BENCHMARK_CAPTURE(BM_xpr, PATTERN, PATTERN ## _xpr, COUNT);  \
 BENCHMARK_CAPTURE(BM_boost, PATTERN, PATTERN, COUNT);        \
 BENCHMARK_CAPTURE(BM_pcre2, PATTERN, PATTERN, COUNT);        \
 BENCHMARK_CAPTURE(BM_pcre2jit, PATTERN, PATTERN, COUNT);     \
