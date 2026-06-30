64fa9fd0ba23320056c323a5208afdcad01fbe1d
wildp <git@wildp.net>
2026-06-30 17:58:31 +0100

    Improve dfa printer

--- tools/dump_fsm.hpp
+++ tools/dump_fsm.hpp
@@ -10,6 +10,8 @@
 
 #include <srx/fsm/tdfa.hpp>
 
+#include "graph_export.hpp"
+
 
 namespace srx::tools {
 
@@ -19,7 +21,7 @@ void dump_fsm(T target, const detail::tagged_dfa<CharT>& dfa)
 {
     using namespace detail::tdfa;
 
-    auto print_regop = [](T target, const regop& op, std::string_view indent = ""){
+    auto print_regop = [&](const regop& op, std::string_view indent = ""){
         if (auto* set = get_if<regop::set>(&op.op); set != nullptr)
             std::println(target, "{}r{} <- {}", indent, op.dst, (set->val) ? 'p' : 'n');
         else if (auto* copy = get_if<regop::copy>(&op.op); copy != nullptr)
@@ -28,39 +30,61 @@ void dump_fsm(T target, const detail::tagged_dfa<CharT>& dfa)
             std::unreachable();
     };
 
+    auto print_offset_cont = [&]( std::uint_least16_t final_offset, std::optional<continue_at_t> continue_at = {}){
+        if (final_offset != 0)
+            std::print(target, " offset={}", -final_offset);
+        if (continue_at.has_value() and *continue_at != no_continue)
+            std::print(target, " cont={}", *continue_at);
+    };
+
+    auto print_regop_blk = [&](std::size_t op_index, std::string_view indent = ""){
+        if (op_index != no_transition_regops)
+            std::print(target, " regops=blk{}:", op_index);
+        std::println(target);
+
+        for (const auto& op : dfa.get_regops(op_index))
+            print_regop(op, indent);
+    };
+
     for (std::size_t i{ 0 }, i_end{ dfa.node_count() }; i < i_end; ++i)
     {
         if (dfa.final_nodes().contains(i))
-            std::println(target, "Node {}: (ACCEPTING)", i);
+            std::println(target, "State q{}: (a)", i);
         else
-            std::println(target, "Node {}:", i);
+            std::println(target, "State q{}:", i);
 
-        for (const auto& tr : dfa.get_node(i).tr)
+        const auto& node = dfa.get_node(i);
+
+        for (const auto& tr : node.tr)
         {
-            std::print("\t{} -> Node {}:", tr.cs.get_intervals(), tr.next);
-
-            if (tr.op_index != no_transition_regops)
-                std::println(target, " [Block {}]", tr.op_index);
+            if constexpr (std::same_as<CharT, char>)
+                std::print("\t{} -> q{}:", make_pretty_charset_string(tr.cs), tr.next);
             else
-                std::println(target);
+                std::print("\t{} -> q{}:", tr.cs.get_intervals(), tr.next);
 
-            for (const auto& op : dfa.get_regops(tr.op_index))
-                print_regop(target, op, "\t\t");
+            print_regop_blk(tr.op_index, "\t\t");
+        }
+
+        if (node.default_tr.has_value())
+        {
+            if (const auto op_index = node.default_tr->op_index; op_index == detail::tdfa::default_transition_is_not_state)
+            {
+                std::println(target, "\tFALLTHROUGH to q{}", node.default_tr->next);
+            }
+            else
+            {
+                std::print(target, "\t. -> q{}:", i, node.default_tr->next);
+                print_regop_blk(node.default_tr->op_index, "\t\t");
+            }
         }
 
         if (dfa.final_nodes().contains(i))
         {
             const auto& fni = dfa.final_nodes().at(i);
 
-            std::print(target, "\t'' -> ACCEPT:");
-            if (fni.final_offset != 0)
-                std::print(target, " [Offset {}]", fni.final_offset);
-            if (fni.op_index != no_transition_regops)
-                std::print(target, " [Block {}]", fni.op_index);
-            std::println(target);
-
-            for (const auto& op : dfa.get_regops(fni.op_index))
-                print_regop(target, op, "\t\t");
+            std::print(target, "\tACCEPT:");
+            print_offset_cont(fni.final_offset);
+            print_regop_blk(fni.op_index, "\t\t");
         }
 
         if (dfa.fallback_nodes().contains(i))
@@ -68,22 +92,14 @@ void dump_fsm(T target, const detail::tagged_dfa<CharT>& dfa)
             const auto& fni = dfa.final_nodes().at(i);
             const auto& fbni = dfa.fallback_nodes().at(i);
 
-            std::print(target, "\tFALLBACK -> ACCEPT:");
-            if (fni.final_offset != 0)
-                std::print(target, " [Offset {}]", fni.final_offset);
-            if (fbni.continue_at != no_continue)
-                std::print(target, " [Continue {}]", fbni.continue_at);
-            if (fbni.op_index != no_transition_regops)
-                std::print(target, " [Block {}]", fbni.op_index);
-            std::println(target);
-
-            for (const auto& op : dfa.get_regops(fbni.op_index))
-                print_regop(target, op, "\t\t");
+            std::print(target, "\tFALLBACK:");
+            print_offset_cont(fni.final_offset, fbni.continue_at);
+            print_regop_blk(fbni.op_index, "\t\t");
         }
     }
 
-    std::println(target, "Continue states: {}", dfa.continue_nodes());
     std::println(target, "Final Registers: {}", dfa.final_registers());
+    std::println(target, "Continue States: {}", dfa.continue_nodes());
 }
 
 template<typename CharT>
